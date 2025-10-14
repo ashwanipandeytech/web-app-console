8629d06ed95fd4e6b501d19ff7d9f0ff28faf3af
Ashwani <ashwani.pandey@myqsoft.com>
2025-10-14 21:52:59 +0530

    done

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,9 @@
 ashwani@ashwani:/opt/android-studio/bin$ cd /opt/android-studio/bin
 ashwani@ashwani:/opt/android-studio/bin$ source ~/.bashrc
 ashwani@ashwani:/opt/android-studio/bin$ ./studio.sh
+
+cd /opt/android-studio/bin  && ./studio.sh
+ng build mobile-app && npx cap copy && npx cap open android
+ng build mobile-app
+npx cap copy
+npx cap open android
