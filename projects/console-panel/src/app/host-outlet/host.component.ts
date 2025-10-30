299cceaec67f413299da60d2b4d2d47600ea6f1e
pushpendra-dwivedi <pushpendradwivedi9@gmail.com>
2025-10-30 22:49:33 +0530

    Done

--- projects/console-panel/src/app/host-outlet/host.component.ts
+++ projects/console-panel/src/app/host-outlet/host.component.ts
@@ -24,7 +24,7 @@ export class HostOutletComponent {
     //this.allInOnePageSections=response?.pageData;
     response?.pageData.map((item: any) => {  
         let  templateVersion=item.fields.cComponentCode
-       this.pageComponentFactory.loadComponentInstance('Block-C-22-v1', item, this.hostContainer);
+      //  this.pageComponentFactory.loadComponentInstance('Block-C-22-v1', item, this.hostContainer);
 
       })
       
