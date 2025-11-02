62ccf1d91e11b34991567d4c99071aff44801e96
Ashwani <ashwani.pandey@myqsoft.com>
2025-11-02 23:09:20 +0530

    done

---
+++ dist/mobile-app/browser/chunk-F3RIWKBI.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-YAKIF5DK.js";import"./chunk-VUBT7MQ3.js";export{a as blockC21v1Component};
---
+++ dist/shared-lib/index.d.ts
@@ -0,0 +1,8 @@
+import * as i0 from '@angular/core';
+
+declare class PageContentFetcher {
+    static ɵfac: i0.ɵɵFactoryDeclaration<PageContentFetcher, never>;
+    static ɵprov: i0.ɵɵInjectableDeclaration<PageContentFetcher>;
+}
+
+export { PageContentFetcher };
