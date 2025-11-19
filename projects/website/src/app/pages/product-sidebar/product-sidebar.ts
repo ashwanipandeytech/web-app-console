44bcd5aa24b2f46a8746234c9b7c10285b642a1d
pushpendra-dwivedi <pushpendradwivedi9@gmail.com>
2025-11-19 08:52:21 +0530

    DOne

--- projects/website/src/app/pages/product-sidebar/product-sidebar.ts
+++ projects/website/src/app/pages/product-sidebar/product-sidebar.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { catchError, of } from 'rxjs';
+import { DataService } from 'shared-lib';
 
 @Component({
   selector: 'web-product-sidebar',
@@ -7,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrl: './product-sidebar.scss'
 })
 export class ProductSidebar {
+public dataService:any= inject(DataService);
+  productListData: any=[];
 
+  constructor(private cd:ChangeDetectorRef){
+    this.callAllProductList();
+  }
+
+  callAllProductList() {
+
+    // const payload = {
+    //   email: this.email,
+    //   password: this.password
+    // };
+
+    
+    this.dataService.callGetApi('products/search','web').pipe(
+      catchError((error) => {
+        // console.error('Error occurred during login:', error);
+       //add toaserfnc alert('Login failed: ' + response.message);
+        // Optionally, you can return an observable to prevent further execution
+        return of(null); // or you can return a default value if needed
+      })
+    ).subscribe((response: any) => {
+      // console.log('Response:', response);
+    this.productListData = response.data.data;
+    this.cd.detectChanges();
+      if (response && response.success) {
+      
+      } else if (response) {
+       //add toaserfnc alert('Login failed: ' + response.message);
+      }
+    });
+    
+  }
 }
