09e3257487f23625d60a8c6860479c9a9c927545
pushpendra-dwivedi <pushpendradwivedi9@gmail.com>
2025-12-07 14:49:34 +0530

    Done

---
+++ projects/frontend/src/app/pages/thankyou/thankyou.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'web-thankyou',
+  imports: [],
+  templateUrl: './thankyou.html',
+  styleUrl: './thankyou.scss',
+})
+export class Thankyou {
+
+}
