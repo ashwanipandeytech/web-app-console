d28d0f3adc7fac8c554db61e0ad67423278002a1
Tushar sharma <tushar@myqsoft.com>
2025-12-18 21:27:37 +0530

    merged

--- projects/frontend/src/app/account/wishlist/wishlist.ts
+++ projects/frontend/src/app/account/wishlist/wishlist.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { DataService } from 'projects/setting-component-shared-lib/src/lib/services/data-service';
+import { GlobaCommonlService } from 'projects/global-common.service';
+
 
 @Component({
   selector: 'web-wishlist',
@@ -9,6 +11,7 @@ import { DataService } from 'projects/setting-component-shared-lib/src/lib/servi
 })
 export class Wishlist {
   private dataService = inject(DataService);
+  private globalCommonService = inject(GlobaCommonlService)
   private cd = inject(ChangeDetectorRef);
   wishListData: any=[];
 ngOnInit(){
@@ -19,6 +22,17 @@ ngOnInit(){
 getWishlistData(){
   this.dataService.get('wishlist').subscribe((res:any)=>{
   this.wishListData = res.data;
+  this.cd.markForCheck();
+  })
+}
+removeWishlist(id:any){
+  this.dataService.delete('wishlist',id).subscribe((res:any)=>{
+    let response = {
+        message:'Item Removed from Wish List',
+        success:true
+    }
+  this.globalCommonService.showMsgSnackBar(response);
+  this.getWishlistData();
   this.cd.markForCheck();
   })
 }
