51b86cdaf25d7d1b5b8c62ba790c42296513d1ce
Aman <mail2aman168@gmail.com>
2026-01-02 00:14:49 +0530

    home page done

--- projects/shared-lib/src/lib/components/hero-section-v1/hero-section-v1.component.ts
+++ projects/shared-lib/src/lib/components/hero-section-v1/hero-section-v1.component.ts
@@ -1,34 +1,29 @@
 import { NgOptimizedImage, NgClass } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
-import { SlickCarouselModule  } from 'ngx-slick-carousel';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-hero-section-v1',
   templateUrl: './hero-section-v1.component.html',
   imports: [SlickCarouselModule, NgClass],
-  styleUrls: ['./hero-section-v1.component.scss']
+  styleUrls: ['./hero-section-v1.component.scss'],
 })
 export class HeroSectionV1Component implements OnInit {
-@Input() data: any;
-// slides = [
-
-//   {img: '/images/Making-Livestock-Care-Simple-Safe-Effective-7-min.png'},
-//   {img:'/images/Making-Livestock-Care-Simple-Safe-Effective-16-min.png'},
-//   ];
-slides:any;
-heroBannerSlideConfig = {
+  @Input() data: any;
+  slides: any;
+  heroBannerSlideConfig = {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
     dots: true,
+    arrows: false,
     lazyLoad: 'ondemand',
   };
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    console.log('HeroSectionV1Component load',this.data);
-    this.slides = this.data.data.heroImage
+    console.log('HeroSectionV1Component load', this.data);
+    this.slides = this.data.data.heroImage;
   }
-
 }
