916237bf6d191384199c69b9b12ec55d99ff32bb
Ashwani <ashwani.pandey@myqsoft.com>
2026-01-19 22:55:27 +0530

    done

--- projects/frontend/src/server.ts
+++ projects/frontend/src/server.ts
@@ -51,16 +51,26 @@ app.use((req, res, next) => {
  * Start the server if this module is the main entry point.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
-// if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
-    if (error) {
-      throw error;
-    }
+if (isMainModule(import.meta.url)) {
+  // const port = process.env['PORT'] || 4000;
+  // app.listen(port, (error) => {
+  //   if (error) {
+  //     throw error;
+  //   }
 
-    //console.log(`Node Express server listening on http://localhost:${port}`);
-  });
-// }
+  //   //console.log(`Node Express server listening on http://localhost:${port}`);
+  // });
+  const port = Number(process.env['PORT']); // mandatory, do not fallback
+
+if (!port) {
+  throw new Error('PORT is not defined by Hostinger Node.js manager');
+}
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Node Express server listening on port ${port}`);
+});
+  
+}
 
 /**
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
