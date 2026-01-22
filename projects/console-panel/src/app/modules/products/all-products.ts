9bc60637b0e6f90bc333ce4144c76685c05d090a
pushpendra-dwivedi <pushpendradwivedi9@gmail.com>
2026-01-22 23:45:16 +0530

    Done

--- projects/console-panel/src/app/modules/products/all-products.ts
+++ projects/console-panel/src/app/modules/products/all-products.ts
@@ -1,94 +1,124 @@
-import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, ViewChild } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { ActivatedRoute, Router } from '@angular/router';
 import { catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { DataService } from 'shared-lib';
-import {Sidebar} from "../../layout/sidebar/sidebar";
-import {Header} from "../../layout/header/header";
+import { Sidebar } from '../../layout/sidebar/sidebar';
+import { Header } from '../../layout/header/header';
 import { ConfirmationPopupComponent } from '../../confirmationPopup/confirmationPopup.component';
 import { MatDialog } from '@angular/material/dialog';
 import { GlobalService } from '../../global.service';
 import { AddProduct } from './add-product/add-product';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { environment } from 'shared-lib/environments/environment';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { DatePipe } from '@angular/common';
+
+// import { MatPaginator } from 'node_modules/@angular/material/paginator.d';
+// import { MatSort } from 'node_modules/@angular/material/sort.d';
 @Component({
   selector: 'app-all-products',
-  imports: [],
+  imports: [DatePipe, MatTableModule, MatPaginatorModule, MatSort, MatCheckboxModule],
   templateUrl: './all-products.html',
-  styleUrl: './all-products.scss'
+  styleUrl: './all-products.scss',
 })
-
-
-
 export class AllProducts {
-
-  public dataService:any= inject(DataService);
-  private activatedRoute= inject(ActivatedRoute);
+  public dataService: any = inject(DataService);
+  private activatedRoute = inject(ActivatedRoute);
   readonly dialog = inject(MatDialog);
   readonly ngbModal = inject(NgbModal);
-  public router=inject(Router)
- //add toastr library private activatedRoute= inject(ActivatedRoute);
-  email:any='superadmin@demohandler.com'
-  password:any='R9!hQ7k$2Pm@A1eZx4LwT8uV#cN0sBf'
-  productListData: any=[];
+  public router = inject(Router);
+  //add toastr library private activatedRoute= inject(ActivatedRoute);
+  email: any = 'superadmin@demohandler.com';
+  password: any = 'R9!hQ7k$2Pm@A1eZx4LwT8uV#cN0sBf';
+  // productListData: any = [];
   defaultPage = 1;
-  constructor(private cd:ChangeDetectorRef,private globalService:GlobalService) {
-  this.callAllProductList()
+
+  displayedColumns: string[] = [
+    'select',
+    'product',
+    'sku',
+    'stock',
+    'price',
+    'sales',
+    'published',
+    'action',
+  ];
+
+  dataSource = new MatTableDataSource<any>();
+  @ViewChild(MatPaginatorModule) paginator!: MatPaginatorModule;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(
+    private cd: ChangeDetectorRef,
+    private globalService: GlobalService,
+  ) {
+    this.callAllProductList();
+  }
+
+  ngAfterViewInit() {
+    // this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
   callAllProductList() {
-this.productListData = [];
+    // this.productListData = [];
     const payload = {
       email: this.email,
-      password: this.password
+      password: this.password,
     };
-    this.dataService.get(`products?page=${this.defaultPage}`).pipe(
-      catchError((error) => {
-        console.error('Error occurred during login:', error);
-       //add toaserfnc alert('Login failed: ' + response.message);
-        // Optionally, you can return an observable to prevent further execution
-        return of(null); // or you can return a default value if needed
-      })
-    ).subscribe((response: any) => {
-      //console.log('Response:', response);
-    this.productListData = response.data;
-    this.cd.detectChanges();
-      if (response && response.success) {
-      
-      } else if (response) {
-       //add toaserfnc alert('Login failed: ' + response.message);
-      }
+    this.dataService
+      .get(`products?page=${this.defaultPage}`)
+      .pipe(
+        catchError((error) => {
+          console.error('Error occurred during login:', error);
+          //add toaserfnc alert('Login failed: ' + response.message);
+          // Optionally, you can return an observable to prevent further execution
+          return of(null); // or you can return a default value if needed
+        }),
+      )
+      .subscribe((response: any) => {
+        console.log('Response:', response);
+        // this.productListData = response.data;
+        this.dataSource.data = response.data.data;
+        this.cd.detectChanges();
+        if (response && response.success) {
+        } else if (response) {
+          //add toaserfnc alert('Login failed: ' + response.message);
+        }
+      });
+  }
+  nextPage(page: any) {
+    this.defaultPage = page;
+    this.callAllProductList();
+  }
+  openAddProductModal(action: any) {
+    const dialogRef: NgbModalRef = this.ngbModal.open(AddProduct, {
+      windowClass: 'mobile-modal product-edit-popup',
+      scrollable: true,
+      centered: true,
+      size: 'xl',
+      backdrop: 'static', // optional
     });
-    
+    dialogRef.componentInstance.data = action;
   }
-  nextPage(page:any){
-this.defaultPage = page;
-this.callAllProductList();
-  }
-  openAddProductModal(action:any){
-     const dialogRef: NgbModalRef = this.ngbModal.open(AddProduct, {
-    windowClass: 'mobile-modal product-edit-popup',
-    scrollable: true,
-    centered: true,
-    size:'xl',
-    backdrop: 'static' // optional
-  });
-  dialogRef.componentInstance.data = action;
-  }
-editoProduct(item:any){
-  console.log('item==>',item);
-  
-   const dialogRef: NgbModalRef = this.ngbModal.open(AddProduct, {
-    windowClass: 'mobile-modal product-edit-popup',
-    scrollable: true,
-    centered: true,
-    size:'xl',
-    backdrop: 'static' // optional
-  });
-  dialogRef.componentInstance.data = item;
+  editoProduct(item: any) {
+    console.log('item==>', item);
 
-       dialogRef.result
+    const dialogRef: NgbModalRef = this.ngbModal.open(AddProduct, {
+      windowClass: 'mobile-modal product-edit-popup',
+      scrollable: true,
+      centered: true,
+      size: 'xl',
+      backdrop: 'static', // optional
+    });
+    dialogRef.componentInstance.data = item;
+
+    dialogRef.result
       .then((result) => {
         console.log('Modal closed with result:', result);
         if (result == 'success') {
@@ -98,53 +128,52 @@ editoProduct(item:any){
       .catch((reason) => {
         //console.log('Modal dismissed:', reason);
       });
-}
+  }
 
-    openDialog(id: any): void {
-      let popupData = {
-        title: 'Product',
-        description: 'Are you sure, you want to delete Product',
-        id: id
+  openDialog(id: any): void {
+    let popupData = {
+      title: 'Product',
+      description: 'Are you sure, you want to delete Product',
+      id: id,
+    };
+    let dialogRef = this.dialog.open(ConfirmationPopupComponent, {
+      width: '250px',
+      data: popupData,
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      //console.log('Dialog closed with:', result);
+
+      if (result.action === 'delete') {
+        // Perform delete
+        this.deleteProduct(result.id);
       }
-      let dialogRef = this.dialog.open(ConfirmationPopupComponent, {
-        width: '250px',
-        data: popupData,
-      });
-      dialogRef.afterClosed().subscribe(result => {
-        //console.log('Dialog closed with:', result);
-  
-        if (result.action === 'delete') {
-          // Perform delete
-          this.deleteProduct(result.id);
-  
-        }
-      })
-    }
-    previewProduct(id:any){
-      let url = `${environment.WEB_URL}/product-details/${id}?preview=1`
-window.open(url,'_blank')
-    }
-      deleteProduct(id: any) {
-    this.dataService.delete(`products/${id}`)
+    });
+  }
+  previewProduct(id: any) {
+    let url = `${environment.WEB_URL}/product-details/${id}?preview=1`;
+    window.open(url, '_blank');
+  }
+  deleteProduct(id: any) {
+    this.dataService
+      .delete(`products/${id}`)
       .pipe(
-        catchError(err => {
+        catchError((err) => {
           console.error('Error:', err);
-           setTimeout(() => {
-          this.globalService.showMsgSnackBar(err);
-        }, 100);
+          setTimeout(() => {
+            this.globalService.showMsgSnackBar(err);
+          }, 100);
           return of(null);
-        })
+        }),
       )
-      
+
       .subscribe((res: any) => {
         //console.log('Response:', res);
         this.callAllProductList();
-          setTimeout(() => {
+        setTimeout(() => {
           this.globalService.showMsgSnackBar(res);
         }, 100);
         this.cd.detectChanges();
         // this.categoryListData = res.data;
       });
   }
-
 }
