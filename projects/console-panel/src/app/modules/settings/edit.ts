06124bfc33b00d2f228187d30e3e6bb6c8979f28
Ashwani <ashwani.pandey@myqsoft.com>
2026-01-25 22:27:56 +0530

    done

--- projects/console-panel/src/app/modules/settings/edit.ts
+++ projects/console-panel/src/app/modules/settings/edit.ts
@@ -11,7 +11,12 @@ import { GlobalService } from '../../global.service';
 import { QuillModule } from 'ngx-quill';
 import Quill from 'quill';
 import { ImageHandler, Options } from 'ngx-quill-upload';
+import htmlEditButton from 'quill-html-edit-button';
+Quill.register('modules/htmlEditButton', htmlEditButton);
 Quill.register('modules/imageHandler', ImageHandler);
+
+
+
 @Component({
   selector: 'app-editpage',
   templateUrl: './edit.html',
@@ -26,74 +31,9 @@ export class EditPage implements OnInit {
   private router: Router = inject(Router);
   settings: any = FormGroup;
   newPage: any
+  modules = {};
+  options: any
 
-  modules = {
-    toolbar: [
-      // --- TEXT STYLE ---
-      [{ 'font': [] }, { 'size': ['small', false, 'large', 'huge'] }],
-      ['bold', 'italic', 'underline', 'strike'],
-      [{ 'color': [] }, { 'background': [] }],
-      [{ 'script': 'sub' }, { 'script': 'super' }],
-
-      // --- HEADER & QUOTES ---
-      [{ 'header': 1 }, { 'header': 2 }, 'blockquote', 'code-block'],
-
-      // --- LISTS & INDENTS ---
-      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-      [{ 'indent': '-1' }, { 'indent': '+1' }],
-      [{ 'direction': 'rtl' }],
-
-      // --- ALIGNMENT ---
-      [{ 'align': [] }],
-
-      // --- LINKS & MEDIA ---
-      ['link', 'image', 'formula'],
-
-      // --- UTILS ---
-      ['clean']
-    ],
-    imageHandler: {
-      upload: (file: File) => {
-       
-        return new Promise((resolve, reject) => {
-          
-          // 1. Validation Logic
-          if (!['image/jpeg', 'image/png', 'image/jpg'].includes(file.type)) {
-             console.info('Unsupported type');
-            return reject('Unsupported type');
-          }
-          if (file.size > 1000000) {
-              console.info('Size');
-            return reject('Size too large (Max 1MB)');
-          }
-
-          // 2. Call the Service
-          const formData = new FormData();
-          formData.append('files', file);
-          formData.append('type', 'pages');
-
-      this.dataService
-      .postForm('gallery', formData)
-       .pipe(
-        catchError((err) => {
-          return of(null);
-        })
-      )
-      .subscribe((res: any) => {
-        console.log('Response:', res);
-        resolve(res.data[0].url);
-     
-      });
-
-        });
-      },
-      accepts: ['png', 'jpg', 'jpeg', 'jfif']
-    } as Options,
-   
-
-
-
-  };
 
   constructor(private cdr: ChangeDetectorRef, private globalService: GlobalService) {
 
@@ -101,6 +41,80 @@ export class EditPage implements OnInit {
 
 
   ngOnInit() {
+
+    this.modules = {
+
+      htmlEditButton: {
+        debug: true, // Developers love logs
+        msg: "Edit HTML", // Tooltip
+        okText: "Save",
+      },
+      toolbar: [
+        // --- TEXT STYLE ---
+        [{ 'font': [] }, { 'size': ['small', false, 'large', 'huge'] }],
+        ['bold', 'italic', 'underline', 'strike'],
+        [{ 'color': [] }, { 'background': [] }],
+         [{ 'script': 'sub' }, { 'script': 'super' }],
+
+        // --- HEADER & QUOTES ---
+        [{ 'header': 1 }, { 'header': 2 }, 'blockquote'],
+
+        // --- LISTS & INDENTS ---
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+        [{ 'indent': '-1' }, { 'indent': '+1' }],
+        [{ 'direction': 'rtl' }],
+
+        // --- ALIGNMENT ---
+        [{ 'align': [] }],
+
+        // --- LINKS & MEDIA ---
+        ['link', 'image', 'formula'],
+        ['htmlEditButton'],
+        // --- UTILS ---
+        ['clean']
+      ],
+      imageHandler: {
+        upload: (file: File) => {
+
+          return new Promise((resolve, reject) => {
+
+            // 1. Validation Logic
+            if (!['image/jpeg', 'image/png', 'image/jpg'].includes(file.type)) {
+              console.info('Unsupported type');
+              return reject('Unsupported type');
+            }
+            // if (file.size > 1000000) {
+            //     console.info('Size');
+            //   return reject('Size too large (Max 1MB)');
+            // }
+
+            // 2. Call the Service
+            const formData = new FormData();
+            formData.append('files', file);
+            formData.append('type', 'pages');
+
+            this.dataService
+              .postForm('gallery', formData)
+              .pipe(
+                catchError((err) => {
+                  return of(null);
+                })
+              )
+              .subscribe((res: any) => {
+                console.log('Response:', res);
+                resolve(res.data[0].url);
+
+              });
+
+          });
+        },
+        accepts: ['png', 'jpg', 'jpeg', 'jfif']
+      } as Options,
+
+
+
+
+    };
     let slug = this.route.snapshot.params['id']
 
     this.dataService.get('pages/' + slug)
@@ -121,6 +135,63 @@ export class EditPage implements OnInit {
 
       });
 
+  }
+
+
+  handleImageUpload(file: any) {
+    console.info('files', file)
+
+    // 2. Call the Service
+    const formData = new FormData();
+    formData.append('files', file);
+    formData.append('type', 'pages');
+    console.info('herere test', formData)
+    this.dataService
+      .postForm('gallery', formData)
+      .pipe(
+        catchError((err) => {
+          return of(null);
+        })
+      )
+      .subscribe((res: any) => {
+        console.log('Response:', res);
+        // resolve(res.data[0].url);
+
+      });
+
+    return
+
+    return new Promise((resolve, reject) => {
+
+      // 1. Validation Logic
+      if (!['image/jpeg', 'image/png', 'image/jpg'].includes(file.type)) {
+        console.info('Unsupported type');
+        return reject('Unsupported type');
+      }
+      if (file.size > 1000000) {
+        console.info('Size');
+        return reject('Size too large (Max 1MB)');
+      }
+
+      // 2. Call the Service
+      const formData = new FormData();
+      formData.append('files', file);
+      formData.append('type', 'pages');
+      console.info('herere test', formData)
+      this.dataService
+        .postForm('gallery', formData)
+        .pipe(
+          catchError((err) => {
+            return of(null);
+          })
+        )
+        .subscribe((res: any) => {
+          console.log('Response:', res);
+          resolve(res.data[0].url);
+
+        });
+
+    });
   }
 
   updatePage() {
