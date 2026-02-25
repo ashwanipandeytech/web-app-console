8245e6539d7126df8028d058dac8d9a7685df01b
Tushar sharma <tushar@myqsoft.com>
2026-02-25 22:19:14 +0530

    merged

--- projects/console-panel/src/app/modules/category/category.ts
+++ projects/console-panel/src/app/modules/category/category.ts
@@ -58,6 +58,7 @@ unloadNotification($event: BeforeUnloadEvent) {
   }
   onImageChange(event: any): void {
     const file = event.target.files?.[0];
+    
     if (file) {
       this.addCategory.patchValue({ categoryThumbnail: file });
     this.addCategory.get('categoryThumbnail')?.updateValueAndValidity();
@@ -173,9 +174,26 @@ unloadNotification($event: BeforeUnloadEvent) {
     })
   }
   editCategory(item:any){
-    this.isEdit = true;
+    this.imagePreview = '';
+     this.isEdit = true;
     this.updateCategoryId = item.id;
-  this.addCategory.patchValue(item);
+    const thumbControl = this.addCategory.get('categoryThumbnail');
+    thumbControl?.clearValidators();
+    thumbControl?.updateValueAndValidity();
+    this.imagePreview = item.thumbnail; 
+    this.imageFile = null;
+    if (this.fileInput) {
+      this.fileInput.nativeElement.value = '';
+    }
+  // this.addCategory.patchValue(item);
+   this.addCategory.patchValue({
+      name: item?.name ?? '',
+      categoryThumbnail: null,
+      customCategoryIcon: item?.customCategoryIcon ?? '',
+      description: item?.description ?? '',
+      is_menu: !!Number(item?.is_menu),
+      allow_on: item?.allow_on ?? ''
+    });
   }
   updatecategory(){
     // let formData = this.addCategory.value;
@@ -248,7 +266,7 @@ else{
       });
   }
   deleteCategory(id: any) {
-    this.dataService.delete('categories', id)
+    this.dataService.delete(`categories/${id}`)
       .pipe(
         catchError(err => {
           console.error('Error:', err);
@@ -268,12 +286,23 @@ else{
         // this.categoryListData = res.data;
       });
   }
-  clearForm(){
+  // clearForm(){
+  //   this.addCategory.reset();
+  // if (this.fileInput) {
+  //   this.fileInput.nativeElement.value = '';
+  //   this.imagePreview = '';
+  // }
+  //   this.isEdit = false;
+  // }
+    clearForm(){
     this.addCategory.reset();
+  this.addCategory.get('categoryThumbnail')?.setValidators([Validators.required]);
+  this.addCategory.get('categoryThumbnail')?.updateValueAndValidity();
   if (this.fileInput) {
     this.fileInput.nativeElement.value = '';
     this.imagePreview = '';
   }
+    this.imageFile = null;
     this.isEdit = false;
   }
   get f() {
