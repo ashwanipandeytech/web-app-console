655dd035031fd4c104f2c218fe02f404faf81017
Tushar sharma <tushar@myqsoft.com>
2026-04-06 18:52:21 +0530

    update configuration

--- projects/frontend/environments/farming.environment.ts
+++ projects/frontend/environments/farming.environment.ts
@@ -8,11 +8,9 @@ export const environment = {
 'GLOBAL_AUTH': 'TEST_VALUE',
 'APPLICATION':'CONSOLE',
 'VERSION':'v1',
-// 'API_URL':'https://api.demohandler.in/api/v1/admin/auth/',
-// 'API_URL_NEW':'https://api.demohandler.in/api/v1/admin/',
-'API_URL':'https://api.farmingmart.in/api/v1/admin/',
+'API_URL':'https://api.farmingmart.in/api/v1/',
 'DOMAIN':'https://api.demohandler.in',
-'RAZORPAY_KEY_ID':false,
+'RAZORPAY_KEY_ID':'rzp_test_Rkp1Rm8aI8wDkH',
 'WEB_URL':'https://farmingmart.in/',
 'ADDRESS_API_KEY':'25d261a6a2cc45deb0954b14270ac706'
 
