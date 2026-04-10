c473aef3e0df5f15280782e549a47470b0468a31
Ashwani <ashwani.pandey@myqsoft.com>
2026-04-11 00:16:37 +0530

    stage test

--- projects/frontend/src/app/app.ts
+++ projects/frontend/src/app/app.ts
@@ -71,7 +71,7 @@ export class App {
 
 
   ngAfterViewInit(): void {
-console.info('STAGE CI/CD PROCESS ON PUSH 1')
+console.info('STAGE CI/CD PROCESS ON PUSH ')
 
     this.activatedRoute.queryParams.subscribe(params => {
       if (this.router.url.includes('reset-password') && params['token']) {
